1f80bd80827a9f02d5ca4e48e02ef44dcdd5523a
Olarn U <olarn.u@odds.team>
2021-01-27 05:56:46 +0700

    Initial Commit

---
+++ TryFollow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TryFollow
+//
+//  Created by Olarn U. on 27/1/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
